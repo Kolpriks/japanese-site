c8dfbf16b0adb8683a92d2220c5d7b5cb80ac059
Kolpriks <vladkulikov348@gmail.com>
2023-12-12 01:42:20 +0300

    Some update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 import '../Card/Card.css'
 
 class Card extends React.Component {
-	dicktionary = {'あ': 'a', 'い': 'i', 'う': 'u', 'え': 'e', 'お': 'o', 'か': 'ka', 'き': 'ki', 
+	dictionary = {'あ': 'a', 'い': 'i', 'う': 'u', 'え': 'e', 'お': 'o', 'か': 'ka', 'き': 'ki', 
 	'く': 'ku', 'け': 'ke', 'こ': 'ko', 'さ': 'sa', 'し': 'shi', 'す': 'su', 'せ': 'se', 'そ': 'so', 
 	'た': 'ta', 'ち': 'chi', 'つ': 'tsu', 'て': 'te', 'と': 'to', 'な': 'na', 'に': 'ni', 'ぬ': 'nu',
-	 'ね': 'ne', 'の': 'no', 'は': 'ha', 'ひ': 'hi', 'ふ': 'fu', 'へ': 'he', 'ほ': 'ho', 'ま': 'ma',
-	  'み': 'mi', 'む': 'mu', 'め': 'me', 'も': 'mo', 'や': 'ya', 'ゆ': 'yu', 'よ': 'yo', 'ら': 'ra', 
-		'り': 'ri', 'る': 'ru', 'れ': 're', 'ろ': 'ro'}
+	'ね': 'ne', 'の': 'no', 'は': 'ha', 'ひ': 'hi', 'ふ': 'fu', 'へ': 'he', 'ほ': 'ho', 'ま': 'ma',
+	'み': 'mi', 'む': 'mu', 'め': 'me', 'も': 'mo', 'や': 'ya', 'ゆ': 'yu', 'よ': 'yo', 'ら': 'ra', 
+	'り': 'ri', 'る': 'ru', 'れ': 're', 'ろ': 'ro'}
+
+	getRandomSymbol() {
+		const symbols = Object.keys(this.dictionary);
+		const randomIndex = Math.floor(Math.random() * symbols.length);
+		return symbols[randomIndex];
+	}
+
 	render() {
 		return(
 		<main className="main-page">
 			<div className="question-card">
 
 				<div className="question-card__symbol">
-					<a className="question-card__symbol__text" >{symbol}</a>
+					<a className="question-card__symbol__text" >{this.getRandomSymbol()}</a>
 				</div>
 
 				<div className="question-card__pronaunce">
